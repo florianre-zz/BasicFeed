a17b1c14f2a5fdbbb4c5c6520213d9bb6560e8cc
fre14 <femile@blueyonder.co.uk>
2017-01-15 23:43:29 +0000

    Start validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validate :decription_is_valid
+
+  private
+
+  def decription_is_valid
+    if !description.nil?
+      if (description.length == 0)
+        flash[:error] = "Cannot publish empty post."
+      # elsif condition
+      end
+    end
+  end
 end
