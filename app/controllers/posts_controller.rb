c953dfb6b465256286dca36f42e74b4b1b322b57
fre14 <femile@blueyonder.co.uk>
2017-01-15 11:04:57 +0000

    Enable create new post - save to DB when created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def index
+    @post = Post.new
   end
 
   def show
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Success!"
+      redirect_to post_path(@post)
+    else
+      flash[:error] = "There was a problem..."
+    end
+  end
+
+private
+  def post_params
+      params.require(:post).permit(:description)
+  end
+
 end
